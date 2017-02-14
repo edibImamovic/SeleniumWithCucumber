15e6d11a0da9db65fb6537659051376e291b0bdc
Edib Imamovic <edib.imamovic@gmail.com>
2017-02-14 12:43:57 +0100

    Added resource folder, code reformated and improved.
    Added resource folder, code reformated and improved.

--- src/main/java/CucumberExercise/Features/CucumberExercise1.java
+++ src/main/java/CucumberExercise/Features/CucumberExercise1.java
@@ -4,37 +4,50 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
-
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.io.File;
+import static org.apache.commons.io.FileUtils.copyFile;
 
 /**
  * Created by edibi on 8.2.2017.
+ * <p>
+ * This is simple selenium test with cucumber framework named cucumber exercise 1.
+ * Workflow for this exercise is > select browser in which you need to test page.Naviga to webpage (google) ,
+ * add some text in search box and press search. After test is done there should be image of test in resource folder.
+ * NOTE: Please change path to your path and there is some issues with internet explorer and finding elements.
  */
 public class CucumberExercise1 extends HelperClass {
-
 
     private String chrome = "CHROME";
     private String internetExplorer = "IE";
     private String firefox = "FIREFOX";
-    String webpage= "http://www.laptopmag.com/";
+    private String webpage = "http://www.google.com/";
+    private String imageValidation = "C:\\Users\\edibi\\Desktop\\selenium tests\\CucumberExercise1\\resources\\screenshot1.png";
 
-
-
+    // This method navigate to webpage (google.com) and user is able to select one of the given browsers.
     @Given("^Navigate to google web page$")
     public void webpageNavigation() throws Throwable {
         browserSelector(chrome);
-        driver.navigate().to(webpage);
+        driver.get(webpage);
     }
 
+    // This method select search box add some text and press search button
     @When("^Click on google search$")
     public void clickOnTheBox() throws Throwable {
-
-        driver.findElement(By.cssSelector("span#JgeUcJjl div.headerSearch > form > input")).sendKeys("where is my laptop? :)");
-        driver.findElement(By.cssSelector("span#XiIcqhYC div.console.lastUnit > nav > ul > li:nth-child(6) > a")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 19);
+        driver.findElement(By.xpath("id('lst-ib')")).sendKeys("where is my test? :)");
+        driver.findElement(By.className("sbico-c")).click();
 
     }
 
+    // This method is final method in my test , after validation of test , we take screenshot and put it in resource folder
     @Then("^Validate result of the given search$")
-        public void searchValidaion() throws Throwable {
+    public void searchValidaion() throws Throwable {
+        driver.getCurrentUrl();
+        File screenshotValidator = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        copyFile(screenshotValidator, new File(imageValidation));
         driver.close();
     }
 
--- src/main/java/CucumberExercise/Features/HelperClass.java
+++ src/main/java/CucumberExercise/Features/HelperClass.java
@@ -4,31 +4,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.nio.charset.IllegalCharsetNameException;
 
 /**
  * Created by edibi on 8.2.2017.
+ * Helper class for CucumberExercise1
  */
 public class HelperClass {
 
     WebDriver driver = null;
 
-   public String browserSelector(String selector) throws Throwable {
+    protected String browserSelector(String selector) throws Throwable {
 
-      if(selector.equalsIgnoreCase("CHROME")){
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\edibi\\Desktop\\selenium tests\\CucumberExercise1\\chromedriver.exe"); // change path to folder where you put driver
-          driver = new ChromeDriver();
+        if (selector.equalsIgnoreCase("CHROME")) {
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\edibi\\Desktop\\selenium tests\\CucumberExercise1\\resources\\chromedriver.exe"); // change path to folder where you put driver
+            driver = new ChromeDriver();
             return selector;
-        } else if (selector.equalsIgnoreCase("FIREFOX")){
-            System.setProperty("webdriver.gecko.driver", "C:\\Users\\edibi\\Desktop\\selenium tests\\CucumberExercise1\\geckodriver.exe"); // change path to folder where you put driver
-          driver = new FirefoxDriver();
+        } else if (selector.equalsIgnoreCase("FIREFOX")) {
+            System.setProperty("webdriver.gecko.driver", "C:\\Users\\edibi\\Desktop\\selenium tests\\CucumberExercise1\\resources\\geckodriver.exe"); // change path to folder where you put driver
+            DesiredCapabilities cap = DesiredCapabilities.firefox();
+            cap.setCapability("marionette", true);
+            driver = new FirefoxDriver();
             return selector;
-        } else if (selector.equalsIgnoreCase("IE")){
-          System.setProperty("webdriver.ie.driver", "C:\\Users\\edibi\\Desktop\\selenium tests\\CucumberExercise1\\IEDriverServer.exe"); // change path to folder where you put driver
-          driver = new InternetExplorerDriver();
-          return selector;
-      }
-         throw new IllegalCharsetNameException("Select one of the three browsers CHROME , Firefox or IE"); // if the user do not select any of the given browsers throw a Exception
+        } else if (selector.equalsIgnoreCase("IE")) {
+            System.setProperty("webdriver.ie.driver", "C:\\Users\\edibi\\Desktop\\selenium tests\\CucumberExercise1\\resources\\IEDriverServer.exe"); // change path to folder where you put driver
+            driver = new InternetExplorerDriver();
+            return selector;
+        }
+        throw new IllegalCharsetNameException("Select one of the three browsers CHROME , Firefox or IE"); // if the user do not select any of the given browsers throw a Exception
     }
 }
